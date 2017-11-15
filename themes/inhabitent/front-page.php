cc4fb700bc6d861737434560a98e8fa3d7049a1d
Amelia <ameliagarvin1@gmail.com>
2017-11-15 12:08:15 -0800

    created custom loop in front-page php as well as styled front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -34,8 +34,34 @@ get_header(); ?>
 
 		<?php endif; ?>
 
+		<?php 
+	 $args = array( 'post_type' => 'post', 
+	 'order' => 'DES', 
+	 'posts_per_page' => 3 );
+
+   $product_posts = get_posts( $args ); // returns an array of posts
+  ?>
+ <ul class="stories"> 
+<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+  <li>
+	
+	<?php the_post_thumbnail( 'large', '' )?>
+	<h2> <?php the_title(); ?> </h2>
+	<?php red_starter_posted_on(); ?> /
+	<?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> 
+	<a href="<?php the_permalink(); ?>">Read entry</a>
+	</li>
+
+<?php endforeach; wp_reset_postdata(); ?>
+</ul>
+ 
+
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
+
+<!-- custom loop  -->
+
