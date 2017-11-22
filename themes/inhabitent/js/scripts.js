0ced8fb11d9ed7f6a613c9cd9ef66db26d114432
Amelia <ameliagarvin1@gmail.com>
2017-11-21 22:40:24 -0800

    Added hide and show function to search bar to appear on click event

--- themes/inhabitent/js/scripts.js
+++ themes/inhabitent/js/scripts.js
@@ -1,8 +1,12 @@
 (function ($) {
  
+  $( "#search-field" ).hide();
+
   $( "#search-submit" ).click(function(event) {
     event.preventDefault();
     $( "#search-field" ).focus();
+    $( "#search-field" ).show( 2000 );
+    $( "#target" ).blur();
 });
 
 
