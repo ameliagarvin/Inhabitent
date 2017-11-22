7accf226fa07bc0196ec1806cb661723b4470e8f
Amelia <ameliagarvin1@gmail.com>
2017-11-22 14:47:16 -0800

    Changed the button for search bar to search

--- themes/inhabitent/js/scripts.js
+++ themes/inhabitent/js/scripts.js
@@ -1,13 +1,34 @@
 (function ($) {
  
-  $( "#search-field" ).hide();
+$( "#search-submit" ).on('click', function(e) {
+  e.preventDefault();
 
-  $( "#search-submit" ).click(function(event) {
-    event.preventDefault();
-    $( "#search-field" ).focus($( "#search-field" ).show( 2000 ));
-    $( "#search-field" ).blur();
+  $( "#search-field" ).toggle().focus();
+
+  $(document).keypress(function(){
+      if(event.which == 13){
+       
+          if($("#search-field").val() != ''){
+            $( ".search-form" ).submit();
+          }
+        
+      }
+  });
+
 });
 
+$( "#search-field" ).on('blur', function(){
+  $( "#search-field" ).toggle();
+});
 
+// add toggle function
 })(jQuery);
 
+// $( "#search-field" ).show( 2000 )
+  // $( "#search-field" ).hide();
+
+//   $( "#search-submit" ).click(function(event) {
+//     event.preventDefault();
+//     $( "#search-field" ).focus();
+//     $( "#search-field" ).blur();
+// });
